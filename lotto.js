13db50590fbad278b549dd4f42a9c68d71d0593a
공민서 <rhdalstj8649@gmail.com>
2024-10-30 11:08:45 +0900

    피드백 반영 수정

---
+++ lotto.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const inputField = document.getElementById('input-amount').querySelector('input'); 
+        
+//         const inputValue = inputField.value; 
+//         alert(`입력된 금액: ${inputValue}`); 
+//     });
+// });
